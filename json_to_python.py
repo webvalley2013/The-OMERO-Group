d6a2bcfed30cee82bfd70551e5b5b7fc7b863ed7
Daniele <daniele.gadler@yahoo.it>
2013-07-16 16:26:46 +0200

    Update json_to_python.py

--- json_to_python.py
+++ json_to_python.py
@@ -1,11 +1,33 @@
-import requests
+###Working Omero code
+
+import omero
+from omero.gateway import BlitzGateway
+from omero.rtypes import *
+import omero.scripts as scripts
 import json
+import requests
 
 data = requests.get("http://192.168.205.10/pl.json")
-#data.content displays
 jsonloaded = data.json()
-#json.dumps(jsonloaded)
-#This returns a list, whosefirst element is the desired dictionary.
-print jsonloaded 
+
+def runScript():
+  client = scripts.client('1_json_python.py', """With this script, you can convert Json files into Python dictionaries.
+	In order to run it, just press the Run Script button at the bottom right corner of this window.""",
+	version = "0.1",
+	authors = ["The OMERO Group", ""],
+	institutions = ["WebValley"],
+	contact = "webvalley@fbk.eu",)
+	conn = BlitzGateway(client_obj=client)
+
+
+	print jsonloaded
+
+	client.closeSession()
+
+if __name__ == "__main__":
+	runScript()
+
+
+
 
 
