d49016b94461d49a4ace8f6b371861189ffba064
Gabriele Franch <franch@gmai.com>
2013-07-17 02:46:59 +0200

    implemented webdav helper class

---
+++ ownloader.py
@@ -0,0 +1,90 @@
+__author__ = 'The webvalley Team (Omero group division)'
+from requests import Request, Session
+from lxml import etree
+import utils
+import requests
+
+
+class DAVLoader(object):
+
+    def __init__(self, url, user, password):
+        self.__url = url
+        self.__user = user
+        self.__password = password
+        self.__s = Session()
+
+        self.__req = Request('HEAD', url=self.__url).prepare()
+
+        if self.__user is not None:
+            self.__req.prepare_auth(auth=(self.__user, self.__password))
+
+        if self.__s.send(self.__req).status_code != 200:
+            raise requests.exceptions.HTTPError()
+
+    def __request(self, method, data, path, match_codes):
+        r = self.__req
+        r.prepare_method(method)
+        r.prepare_body(data=data, files=None)
+        r.prepare_url(url=self.__url+path, params=None)
+        return self.__s.send(r).status_code in match_codes
+
+    def __retrieve(self, method, data, path, match_codes):
+        r = self.__req
+        r.prepare_method(method)
+        r.prepare_body(data=data, files=None)
+        r.prepare_url(url=self.__url+path, params=None)
+        resp = self.__s.send(r)
+        if resp.status_code in match_codes:
+            return resp.content
+        return False
+
+    def upload(self, path, data):
+        return self.__request('PUT', data, path, [201])
+
+    def mkdir(self, path):
+        if self.__request('HEAD', None, path, [200]):
+            return True
+        return self.__request('MKCOL', None, path, [201])
+
+    def delete(self, path):
+        return self.__request('DELETE', None, path, [201, 202, 204])
+
+    def download(self, path):
+        return self.__retrieve('GET', None, path, [200])
+
+    def list(self, path):
+        data = '<?xml version="1.0"?><a:propfind xmlns:a="DAV:"><a:allprop/></a:propfind>'
+        xml = self.__retrieve('PROPFIND', data, path, [200, 207])
+        parser = etree.XMLParser(remove_blank_text=True)
+        tree = utils.clear_xml_comments(utils.clear_xml_namespaces(etree.XML(xml, parser)))
+        files = []
+        dirs = []
+        for response in tree.findall('response'):
+            href = response.find('href')
+            if href is not None:
+                dict = {}
+                dict['path'] = href.text
+                try:
+                    props = response.find('propstat').find('prop')
+
+                    lastmod = props.find('getlastmodified')
+                    if lastmod is not None:
+                        dict['lastmod'] = lastmod.text
+
+                    contentlength = props.find('getcontentlength')
+                    if contentlength is not None:
+                        dict['size'] = contentlength.text
+
+                    contenttype = props.find('getcontenttype')
+                    if contenttype is not None:
+                        dict['type'] = contenttype.text
+
+                    if props.find('resourcetype').find('collection') is not None:
+                        dirs.append(dict)
+                    else:
+                        files.append(dict)
+
+                except AttributeError:
+                    pass
+
+        return dirs, files
---
+++ utils.py
@@ -0,0 +1,85 @@
+import os
+import io
+
+from lxml import etree
+
+XSLT_STRIP_NAMESPACES="""
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output method="xml" indent="no"/>
+
+<xsl:template match="/|comment()|processing-instruction()">
+<xsl:copy>
+<xsl:apply-templates/>
+</xsl:copy>
+</xsl:template>
+
+<xsl:template match="*">
+<xsl:element name="{local-name()}">
+<xsl:apply-templates select="@*|node()"/>
+</xsl:element>
+</xsl:template>
+
+<xsl:template match="@*">
+<xsl:attribute name="{local-name()}">
+<xsl:value-of select="."/>
+</xsl:attribute>
+</xsl:template>
+</xsl:stylesheet>
+"""
+
+XSLT_STRIP_COMMENTS="""
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+<xsl:output omit-xml-declaration="yes"/>
+<xsl:strip-space elements="*" />
+<xsl:template match="@*|node()" name="identity">
+<xsl:copy>
+<xsl:apply-templates select="@*|node()"/>
+</xsl:copy>
+</xsl:template>
+<xsl:template match="comment()"/>
+<xsl:template match="@choice">
+<xsl:value-of select="concat(.,'&#x9;')"/>
+</xsl:template>
+<xsl:template match="question|answer">
+<xsl:call-template name="identity"/>
+<xsl:text>&#xA;</xsl:text>
+</xsl:template>
+</xsl:stylesheet>
+"""
+
+def url_join(*args):
+    """
+Join any arbitrary strings into a forward-slash delimited list.
+Do not strip leading / from first element.
+"""
+    if len(args) == 0:
+        return ""
+
+    if len(args) == 1:
+        return str(args[0])
+
+    else:
+        args = [str(arg).replace("\\", "/") for arg in args]
+
+        work = [args[0]]
+        for arg in args[1:]:
+            if arg.startswith("/"):
+                work.append(arg[1:])
+            else:
+                work.append(arg)
+
+        joined = reduce(os.path.join, work)
+        joined = joined.replace("\\", "/")
+
+    return joined[:-1] if joined.endswith("/") else joined
+
+def apply_xslt(xslt, xml_tree):
+    xslt_transform = etree.parse(io.BytesIO(xslt))
+    transform = etree.XSLT(xslt_transform)
+    return transform(xml_tree)
+
+def clear_xml_namespaces(xml_tree):
+    return apply_xslt(XSLT_STRIP_NAMESPACES, xml_tree)
+
+def clear_xml_comments(xml_tree):
+    return apply_xslt(XSLT_STRIP_COMMENTS, xml_tree)
